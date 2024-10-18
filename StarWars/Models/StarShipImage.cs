51b816b2e76ac24c63a148335e057c2275fcb0dc
Keoki Stevenson <keokisteveson@gmail.com>
2024-10-18 19:39:34 -0400

    Create Star Ship Image Model

---
+++ StarWars/Models/StarShipImage.cs
@@ -0,0 +1,10 @@
+﻿namespace StarWars.Models
+{
+    public class StarShipImage
+    {
+        public string Id { get; set; }
+        public string StarShipId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
